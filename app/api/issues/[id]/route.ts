af2b907547e76c44c7c0d1d0140d15fbd77381b3
aznamolkn123 <aznawaditwaiba@gmail.com>
2026-02-09 12:30:37 +0530

    Enhance the api to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,54 +1,75 @@
 import authOptions from "@/app/auth/authOptions";
 import { prisma } from "@/app/lib/prisma";
-import { IssueSchema } from "@/app/validationScemas";
+import { patchIssueSchema } from "@/app/validationScemas";
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 
-export async function PATCH(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
-    const session = await getServerSession(authOptions)
-    if (!session) {
-        return NextResponse.json({}, { status: 401 });
-
-    }
+export async function PATCH(
+    request: NextRequest,
+    { params }: { params: Promise<{ id: string }> },
+) {
+    // const session = await getServerSession(authOptions);
+    // if (!session) {
+    //     return NextResponse.json({}, { status: 401 });
+    // }
     const { id } = await params;
-    const body = await request.json()
-    const validation = IssueSchema.safeParse(body);
+    const body = await request.json();
+    const validation = patchIssueSchema.safeParse(body);
+
     if (!validation.success)
-        return NextResponse.json({ error: validation.error.format() }, { status: 400 });
+        return NextResponse.json(
+            { error: validation.error.format() },
+            { status: 400 },
+        );
 
+    const { assignedToUserId, title, description } = body;
+
+    if (assignedToUserId) {
+        const user = await prisma.user.findUnique({
+            where: { id: assignedToUserId },
+        });
+
+        if (!user) {
+            return NextResponse.json({ error: "Invalid User" }, { status: 400 });
+        }
+    }
     const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(id) }
-    })
-    if (!issue)
-
-        return NextResponse.json({ error: "Issue not found" }, { status: 404 });
-    const updatedIssue = await prisma.issue.update({
         where: { id: parseInt(id) },
+    });
+
+    if (!issue)
+        return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+
+    const updatedIssue = await prisma.issue.update({
+        where: { id: issue.id },
         data: {
-            title: body.title,
-            description: body.description
+            title,
+            description,
+            assignedToUserId: assignedToUserId || null,
+
         }
     })
     return NextResponse.json(updatedIssue)
 
 }
 
-export async function DELETE(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
-    const session = await getServerSession(authOptions)
+export async function DELETE(
+    request: NextRequest,
+    { params }: { params: Promise<{ id: string }> },
+) {
+    const session = await getServerSession(authOptions);
     if (!session) {
         return NextResponse.json({}, { status: 401 });
-
     }
     const { id } = await params;
 
     const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(id) }
-    })
+        where: { id: parseInt(id) },
+    });
     if (!issue)
         return NextResponse.json({ error: "Issue not found " }, { status: 404 });
     await prisma.issue.delete({
-        where: { id: parseInt(id) }
-    })
-    return NextResponse.json({ message: "Issue deleted successfully" })
+        where: { id: parseInt(id) },
+    });
+    return NextResponse.json({ message: "Issue deleted successfully" });
 }
-
--- app/validationScemas.ts
+++ app/validationScemas.ts
@@ -1,6 +1,12 @@
-import z from "@/node_modules/zod/v4/classic/external.cjs";
+import z from "zod";
 
 export const IssueSchema = z.object({
     title: z.string().min(1).max(255),
-    description: z.string().min(1)
+    description: z.string().min(1).max(65535)
+});
+
+export const patchIssueSchema = z.object({
+    title: z.string().min(1).max(255).optional(),
+    description: z.string().min(1).max(65535).optional(),
+    assignedToUserId: z.string().min(1, "AssignedToUserId is required").max(255).optional().nullable()
 });

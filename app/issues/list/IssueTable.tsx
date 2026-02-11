7a3d879207a72e3ac44f4ecfb2153c7631668270
aznamolkn123 <aznawaditwaiba@gmail.com>
2026-02-11 16:03:24 +0530

    Refactor: Extract IssueTable

---
+++ app/issues/list/IssueTable.tsx
@@ -0,0 +1,86 @@
+import Link from "@/app/components/Link";
+import { Issue, Status } from "@/app/generated/prisma/edge";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
+import { Table } from "@radix-ui/themes";
+import NextLink from "next/link";
+import { Suspense } from "react";
+import IssueStatusBadge from "../../components/IssueStatusBadge";
+
+export interface IssueQuery {
+    status: Status;
+    orderBy: keyof Issue;
+    page: string;
+}
+interface Props {
+    searchParams: Promise<IssueQuery>;
+    issues: Issue[];
+}
+
+const IssueTable = async ({ searchParams, issues }: Props) => {
+    const param = await searchParams;
+
+    return (
+        <Suspense fallback={<p>Loading Issues...</p>}>
+            <Table.Root variant="surface" className="max-w-3xl">
+                <Table.Header>
+                    <Table.Row>
+                        {columns.map((column) => (
+                            <Table.ColumnHeaderCell
+                                key={column.value}
+                                className={column.classname}
+                            >
+                                <NextLink
+                                    href={{
+                                        query: { ...param, orderBy: column.value },
+                                    }}
+                                >
+                                    {column.label}
+                                </NextLink>
+                                {column.value === param.orderBy && (
+                                    <ArrowUpIcon className="inline" />
+                                )}
+                            </Table.ColumnHeaderCell>
+                        ))}
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {issues.map((issue) => (
+                        <Table.Row key={issue.id}>
+                            <Table.Cell>
+                                <Link href={`/issues/${issue.id}`}> {issue.title}</Link>
+
+                                <div className="block md:hidden">
+                                    <IssueStatusBadge status={issue.status} />
+                                </div>
+                            </Table.Cell>
+                            <Table.Cell className="hidden md:table-cell">
+                                <IssueStatusBadge status={issue.status} />
+                            </Table.Cell>
+                            <Table.Cell className="hidden md:table-cell">
+                                {issue.createdAt.toDateString()}
+                            </Table.Cell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table.Root>
+        </Suspense>
+    );
+};
+
+const columns: {
+    label: string;
+    value: keyof Issue;
+    classname?: string;
+}[] = [
+        { label: "Issue", value: "title" },
+        { label: "Status", value: "status", classname: "hidden md:table-cell" },
+        {
+            label: "Created At",
+            value: "createdAt",
+            classname: "hidden md:table-cell",
+        },
+    ];
+
+export const columnNames = columns.map((column) => column.value);
+
+export default IssueTable;
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,88 +1,46 @@
-
-import Link from "@/app/components/Link";
-import { Status } from '@/app/generated/prisma/edge';
-import { prisma } from '@/app/lib/prisma';
-import { Issue } from '@prisma/client';
-import { ArrowUpIcon } from '@radix-ui/react-icons';
-import { Table, } from "@radix-ui/themes";
-import NextLink from "next/link";
-import { Suspense } from 'react';
-import IssueStatusBadge from "../../components/IssueStatusBadge";
-import IssueActions from "./IssueActions";
 import Pagination from "@/app/components/Pagination";
+import { Status } from "@/app/generated/prisma/edge";
+import { prisma } from "@/app/lib/prisma";
+import IssueActions from "./IssueActions";
+import IssueTable, { columnNames, IssueQuery } from "./IssueTable";
+import { Flex } from "@radix-ui/themes";
+
 interface Props {
-    searchParams: Promise<{ status: Status, orderBy: keyof Issue, page: string }>
+    searchParams: Promise<IssueQuery>;
 }
 const IssuesPage = async ({ searchParams }: Props) => {
     const param = await searchParams;
-    const columns: {
-        label: string;
-        value: keyof Issue;
-        classname?: string
-    }[] = [
-            { label: "Issue", value: "title" },
-            { label: "Status", value: "status", classname: "hidden md:table-cell" },
-            { label: "Created At", value: "createdAt", classname: "hidden md:table-cell" }
-        ];
+
     const statuses = Object.values(Status);
     const status = statuses.includes(param.status) ? param.status : undefined;
-    const where = { status }
-    const orderBy = columns.map((column) => (column.value))
-        .includes(param.orderBy) ? { [param.orderBy]: "asc" } : undefined;
+    const where = { status };
+    const orderBy = columnNames.includes(param.orderBy)
+        ? { [param.orderBy]: "asc" }
+        : undefined;
 
     const page = parseInt(param.page) || 1;
     const pageSize = 10;
 
-    const issues = await prisma.issue.findMany(
-        {
-            where: where,
-            orderBy: orderBy,
-            skip: (page - 1) * pageSize,
-            take: pageSize
-        },
-
-    );
-    const totalIssues = await prisma.issue.count({ where: where })
+    const issues = await prisma.issue.findMany({
+        where: where,
+        orderBy: orderBy,
+        skip: (page - 1) * pageSize,
+        take: pageSize,
+    });
+    const totalIssues = await prisma.issue.count({ where: where });
 
     return (
-        <div >
+        <Flex direction="column" gap="4">
             <IssueActions />
-            <Suspense fallback={<p>Loading Issues...</p>}>
-                <Table.Root variant="surface" className="max-w-3xl" >
-                    <Table.Header>
-                        <Table.Row>
-                            {columns.map((column) => (
-                                <Table.ColumnHeaderCell key={column.value} className={column.classname}>
-                                    <NextLink href={{
-                                        query: { ...param, orderBy: column.value }
-                                    }}>{column.label}</NextLink>
-                                    {column.value === param.orderBy && <ArrowUpIcon className='inline' />}
-                                </Table.ColumnHeaderCell>
+            <IssueTable searchParams={searchParams} issues={issues} />
+            <Pagination
+                itemCount={totalIssues}
+                pageSize={pageSize}
+                currentPage={page}
+            />
+        </Flex >
+    );
+};
 
-                            ))}
-
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {issues.map((issue) => (
-                            <Table.Row key={issue.id}>
-                                <Table.Cell>
-                                    <Link href={`/issues/${issue.id}`}> {issue.title}</Link>
-
-                                    <div className="block md:hidden"><IssueStatusBadge status={issue.status} /></div>
-                                </Table.Cell>
-                                <Table.Cell className="hidden md:table-cell"><IssueStatusBadge status={issue.status} /></Table.Cell>
-                                <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
-                            </Table.Row>
-                        ))}
-                    </Table.Body>
-                </Table.Root>
-                <Pagination itemCount={totalIssues} pageSize={pageSize} currentPage={page} />
-            </Suspense >
-        </div>
-
-    )
-}
-
-export const dynamic = 'force-dynamic';
-export default IssuesPage
+export const dynamic = "force-dynamic";
+export default IssuesPage;
